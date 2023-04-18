b54952c46e8a4d1203fa9f3d6fcb8177e30a2e65
Mr.KhanVau420(rakib) <67776840+khanbrother420@users.noreply.github.com>
2023-04-18 11:26:19 +0600

    Update Premium.py

--- Premium.py
+++ Premium.py
@@ -332,8 +332,8 @@ def rcrack1(uid,pwx,tl):
             "login":"Log In"}
             #_____Mathoid______#
             headers = {'authority': 'm.alpha.facebook.com',
-    'method": 'GET',
-    'scheme": 'https',
+    'method': 'GET',
+    'scheme': 'https',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'max-age=0',
